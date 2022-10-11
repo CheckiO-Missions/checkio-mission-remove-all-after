0500b7868e68affc6f8a4369b8640f00c075b1fd
Oleksandr Zozulia <freemanlex@gmail.com>
2022-10-11 22:59:17 +0300

    next-api uk

--- verification/tests.py
+++ verification/tests.py
@@ -11,22 +11,22 @@ Each test is a dict with
 TESTS = {
     "Basics": [
         {
-            "input": [[1,2,3,4,5], 3],
-            "answer": [1,2,3],
+            "input": [[1, 2, 3, 4, 5], 3],
+            "answer": [1, 2, 3],
             "explanation": "4 and 5 go after 3, which means should be removed"
         },
         {
-            "input": [[1,1,2,2,3,3], 2],
-            "answer": [1,1,2],
+            "input": [[1, 1, 2, 2, 3, 3], 2],
+            "answer": [1, 1, 2],
             "explanation": "only first '2' counts, the second '2' should be removed"
         },
         {
-            "input": [[1,1,2,4,2,3,4], 2],
-            "answer": [1,1,2],
+            "input": [[1, 1, 2, 4, 2, 3, 4], 2],
+            "answer": [1, 1, 2],
         },
         {
-            "input": [[1,1,5,6,7], 2],
-            "answer": [1,1,5,6,7],
+            "input": [[1, 1, 5, 6, 7], 2],
+            "answer": [1, 1, 5, 6, 7],
             "explanation": "Nothing should be removed if nothing is found"
         },
         {
@@ -35,8 +35,8 @@ TESTS = {
             "explanation": "Empty list stays unchanged"
         },
         {
-            "input": [[7,7,7,7,7,7,7,7,7], 7],
-            "answer": [7]
+            "input": [[7, 7, 7, 7, 7, 7, 7, 7, 7], 7],
+            "answer": [7],
         }
     ],
     "Extra": [
@@ -45,8 +45,8 @@ TESTS = {
             "answer": [10, 1, 5],
         },
         {
-            "input": [[1,2, 6,7,1,2,4,6,7,8,3,5,2,3], 6],
-            "answer": [1,2,6],
+            "input": [[1, 2, 6, 7, 1, 2, 4, 6, 7, 8, 3, 5, 2, 3], 6],
+            "answer": [1, 2, 6],
         }
     ]
 }
